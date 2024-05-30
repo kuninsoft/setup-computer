09ee5a440a622822ce05a182071bc85d017e1a17
Sviatoslav Kunynets <kuninsoft@gmail.com>
2024-05-30 18:46:29 +0300

    Initial Commit

---
+++ Computer Setup/Computer_SetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Computer_SetupApp.swift
+//  Computer Setup
+//
+//  Created by Sviatoslav Kunynets on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Computer_SetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Computer Setup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Computer Setup
+//
+//  Created by Sviatoslav Kunynets on 30.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
